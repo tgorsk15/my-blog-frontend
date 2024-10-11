3ca7a91863b486e69588c3a16b8a115c54a292b2
tgorsk15 <tgbusiness463@gmail.com>
2024-10-11 16:41:11 -0500

    Add login link to NavBar, which uses logout context function

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -4,12 +4,17 @@ import { useAuth } from '../../utils/useAuth'
 import '../../App.css'
 
 export const NavBar = () => {
-    const { userData } = useAuth();
+    const { userData, logout } = useAuth();
     console.log('user in nav bar', userData)
+
+function handleLogout() {
+    logout()
+}
+
 
     return (
         <nav className='navbar-section'>
-            {userData.isAuthor && (
+            {userData && userData.isAuthor === true && (
                 <h2>Hi {userData.firstName}</h2>
             )}
             <div className='links-container'>
@@ -17,14 +22,22 @@ export const NavBar = () => {
                     <li>
                         <Link to="/home">Home</Link>
                     </li>
-                    {userData.isAuthor && (
+                    {userData && userData.isAuthor && (
                         <li>
                             <Link to="/blogDash">Author Hub</Link>
                         </li>
                     )}
-                    <li>
-                        <Link to="/profile">Profile</Link>
-                    </li> 
+                    {userData ? (
+                        <li>
+                            <Link to="/profile" onClick={handleLogout}>Logout</Link>
+                        </li>
+                    
+                    ) : (
+                        <li>
+                            <Link to="/profile">Log In</Link>
+                        </li>
+                    )}
+                     
                 </ul>
                 
             </div>
--- src/utils/authContext.jsx
+++ src/utils/authContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext } from "react";
+import { createContext } from "react";
 import { useState } from "react";
 import PropTypes from "prop-types"
 
