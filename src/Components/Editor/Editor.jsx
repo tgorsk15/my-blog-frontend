7d86c148725674a3659502835e3a7dc51f88ca98
tgorsk15 <tgbusiness463@gmail.com>
2024-10-14 14:43:20 -0500

    integrate Tiny MCE text editor into PostEditor component

--- src/Components/Editor/Editor.jsx
+++ src/Components/Editor/Editor.jsx
@@ -3,6 +3,17 @@ import { Editor } from '@tinymce/tinymce-react'
 export const PostEditor = () => {
 
     return (
-        <Editor />
+        <Editor 
+            apiKey = {import.meta.env.VITE_TINY_API_KEY}
+            init={{
+                plugins:[
+                    'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'image', 'link', 
+                    'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount',
+                ],
+                toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+                content_style: ''
+            }}
+            
+        />
     )
 }
