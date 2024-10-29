0187d006494354ebb80928775f067b67c5a6eceb
tgorsk15 <tgbusiness463@gmail.com>
2024-10-29 10:29:17 -0500

    Restructre and add to Home page

---
+++ src/Components/Search/Search.jsx
@@ -0,0 +1,11 @@
+
+import homeStyles from "../../Pages/Home/home.module.css"
+
+export const Search = () => {
+
+        return (
+            <div className={homeStyles.searchContainer}>
+
+            </div>
+        )
+}
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import { useLoaderData } from "react-router-dom"
 import { useAuth } from "../../utils/useAuth"
 
 import { BlogItem } from "../../Components/BlogItem/BlogItem"
+import { Search } from "../../Components/Search/Search"
 import homeStyles from "./home.module.css"
 
 export const Home = () => {
@@ -10,38 +11,57 @@ export const Home = () => {
     const { pageLoading } = useAuth()
     // const token = localStorage.getItem('token')
     // const userData = JSON.parse(localStorage.getItem('user'))
-    // console.log('user and token info:', userData, token)
 
     return (
-        
+        // TMW 10/29: start building this page out
+        // on right side I will have search bar (include reset button),
+        // and Git/phone number info
+        // on left the user will be able to toggle between "All" and "Latest"
+        // will probably have to create a loading animation on switching between these two
         <section className={homeStyles.homePage}>
-            {pageLoading ? (
-                <div className={homeStyles.spinnerContainer}>
-                    <div className={homeStyles.loadSpinner}>
-                        Hi Im Loading
-                    </div>    
+            <section className={homeStyles.topHomeSection}>
+                <h2>Check out My Posts</h2>
+                <div className={homeStyles.postSelectionBox}>
+                    <button className={homeStyles.defaultPostsBtn}>
+                        All Posts
+                    </button>
+                    <button className={homeStyles.latestPostsBtn}>
+                        Latest Posts
+                    </button>
                 </div>
-            ) : (
-                <div className={homeStyles.postsSection}>
-                    {postsList.length > 0 ? (
-                        postsList.map((post) => {
-                            return (
-                                <BlogItem
-                                    key ={post.id} 
-                                    post={post}
-                                />
-                            )
+            </section>
+            <section className={homeStyles.bottomHomeSection}>
+                <div className={homeStyles.leftSide}>
+                    <div className={homeStyles.postsSection}>
+                        {postsList.length > 0 ? (
+                            postsList.map((post) => {
+                                return (
+                                    <BlogItem
+                                        key ={post.id} 
+                                        post={post}
+                                    />
+                                )
+                                
+                            })
+
                             
-                        })
+                        ) : (
+                            <h2>There is nothing published currently</h2>
+                        )}
+                    </div> 
+                </div>
+                <div className={homeStyles.rightSide}>
+                    {/* this will contain search bar, author info */}
+                    <Search
+                    
+                    />
+                    <div className={homeStyles.infoContainer}>
+                        Hi I'm info
+                    </div>
+                </div> 
+                
 
-                        
-                    ) : (
-                        <p>There is nothing published currently</p>
-                    )}
-                </div>    
-            )}
-            
-
+            </section>
         </section>
     )
 }
