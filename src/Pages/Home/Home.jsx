41e54adbeb723497f770222e8ba81b4b3f296187
tgorsk15 <tgbusiness463@gmail.com>
2024-11-02 14:14:47 -0500

    Make rightSide of Home sticky as the user scrolls

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -42,79 +42,79 @@ export const Home = () => {
             ) : (
                 <>
                 <section className={homeStyles.topHomeSection}>
-                <h2>Check out</h2>
-                <div className={homeStyles.postSelectionBox}>
-                    <button 
-                        className={`${homeStyles.defaultPostsBtn} 
-                            ${activeBtn === 'all' ? homeStyles.activeBtn : ''} `}
-                        onClick={() => {
-                            setBtn('all')
-                            handleGetAll()
-                        }}
-                    >
-                        All Posts
-                    </button>
-                    <button 
-                        className={`{homeStyles.latestPostsBtn}
-                            ${activeBtn === 'latest' ? homeStyles.activeBtn : ''}`}
-                        onClick={handleGetLatest}
-                    >
-                        Latest Posts
-                    </button>
-                </div>
-            </section>
-            <section className={homeStyles.bottomHomeSection}>
-                <div className={homeStyles.leftSide}>
-                    <div className={homeStyles.postsSection}>
-                        {activeList.length > 0 ? (
-                            activeList.map((post) => {
-                                return (
-                                    <BlogItem
-                                        key ={post.id} 
-                                        post={post}
-                                        setLoading={setLoading}
-                                    />
-                                )
-                                
-                            })
- 
-                        ) : (
-                            <div className={homeStyles.emptyPosts}>
-                                <img src={noResults} alt="no results" />  
-                                <h2>No results were found</h2>
-
-                            </div>
-                        )}
-                    </div> 
-                </div>
-                <div className={homeStyles.rightSide}>
-                    {/* contains search bar, author info */}
-                    <Search
-                        postsList = {postsList}
-                        setList = {setList}
-                        handleGetAll = {handleGetAll}
-                    />
-                    <div className={homeStyles.infoContainer}>
-                        <h2 className={homeStyles.contactTitle}>Contact:</h2>
-                        <div className={homeStyles.githubBox}>
-                            <h3>
-                                <a href={import.meta.env.VITE_GITHUB_URL}
-                                    className={homeStyles.githubLink}
-                                >
-                                    <i className="devicon-github-original"></i>
-                                    tgorsk15
-                                </a>
-                            </h3>
-                        </div>
-                        <div className={homeStyles.phoneEmailBox}>
-                            <h3>Phone: {authorPhone}</h3>
-                            <h3>Email: {authorEmail}</h3>
-                        </div>
+                    <h2>Check out</h2>
+                    <div className={homeStyles.postSelectionBox}>
+                        <button 
+                            className={`${homeStyles.defaultPostsBtn} 
+                                ${activeBtn === 'all' ? homeStyles.activeBtn : ''} `}
+                            onClick={() => {
+                                setBtn('all')
+                                handleGetAll()
+                            }}
+                        >
+                            All Posts
+                        </button>
+                        <button 
+                            className={`{homeStyles.latestPostsBtn}
+                                ${activeBtn === 'latest' ? homeStyles.activeBtn : ''}`}
+                            onClick={handleGetLatest}
+                        >
+                            Latest Posts
+                        </button>
                     </div>
-                </div> 
-                
+                </section>
+                <section className={homeStyles.bottomHomeSection}>
+                    <div className={homeStyles.leftSide}>
+                        <div className={homeStyles.postsSection}>
+                            {activeList.length > 0 ? (
+                                activeList.map((post) => {
+                                    return (
+                                        <BlogItem
+                                            key ={post.id} 
+                                            post={post}
+                                            setLoading={setLoading}
+                                        />
+                                    )
+                                    
+                                })
+    
+                            ) : (
+                                <div className={homeStyles.emptyPosts}>
+                                    <img src={noResults} alt="no results" />  
+                                    <h2>No results were found</h2>
 
-            </section>
+                                </div>
+                            )}
+                        </div> 
+                    </div>
+                    <div className={homeStyles.rightSide}>
+                        {/* contains search bar, author info */}
+                        <Search
+                            postsList = {postsList}
+                            setList = {setList}
+                            handleGetAll = {handleGetAll}
+                        />
+                        <div className={homeStyles.infoContainer}>
+                            <h2 className={homeStyles.contactTitle}>Contact:</h2>
+                            <div className={homeStyles.githubBox}>
+                                <h3>
+                                    <a href={import.meta.env.VITE_GITHUB_URL}
+                                        className={homeStyles.githubLink}
+                                    >
+                                        <i className="devicon-github-original"></i>
+                                        tgorsk15
+                                    </a>
+                                </h3>
+                            </div>
+                            <div className={homeStyles.phoneEmailBox}>
+                                <h3>Phone: {authorPhone}</h3>
+                                <h3>Email: {authorEmail}</h3>
+                            </div>
+                        </div>
+                    </div> 
+                    
+
+                </section>
             </>
             )}
             
